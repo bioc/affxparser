acc42189c3199fb30ce12ddfc1a8d170d33160d2
Henrik Bengtsson <hb@maths.lth.se>
2012-03-06 20:05:22 +0000

    Version: 1.26.4 [2012-03-06] o BUG FIX: affxparser would not build on Windows with the new   Rtools toolchain (Rtools 2.15.0.1915-1919), which is for   R (> 2.14.1), i.e. also for R v2.14.2 (but not v2.14.1).   This is the same bug fix that was first done in v1.27.4.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/branches/RELEASE_2_9/madman/Rpacks/affxparser@63550 bc3139a8-67e5-0310-9ffc-ced21a209358

--- src/R_affx_constants.h
+++ src/R_affx_constants.h
@@ -16,3 +16,12 @@
 #include <w32api.h>
 #define WINVER WindowsXP	/* for Util.cpp, via TsvFile.cpp */
 #endif
+
+/* Patch for compilation errors ("'int32_t' does not name a type")
+   on Windows using the Rtools 2.15.0.1915-1919 toolchain, which
+   is used by R (> 2.14.1), cf. private email on 'affxparser (devel)
+   on Windows' on Jan 25 - Mar 5, 2012. */
+#ifdef __MINGW32__
+#include <stdint.h>
+#endif
+
